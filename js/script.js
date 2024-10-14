142ef44f5d44c13abd813cf89f2d9c2031374ca0
Artem <artembaloban21@gmail.com>
2024-10-14 10:52:42 +0300

    first jom in films

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for(let i = 0; i < 2; i++){
+    const filmsWatched = prompt("Один из последних просмотренных фильмов?");
+    const gradeFilm = prompt("Оцените этот фильм?");
+    personalMovieDB.movie[filmsWatched] = gradeFilm;
+};
+
+console.log(personalMovieDB);
