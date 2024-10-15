45fb3f3ed8969eaa3690a13a1e762944ec5126aa
Artem <artembaloban21@gmail.com>
2024-10-15 22:29:50 +0300

    cilce in films

--- js/script.js
+++ js/script.js
@@ -10,10 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
+if(numberOfFilms < 10) {
+    console.log("Просмотрено довольно мало фильмов!");
+} else if(numberOfFilms >= 10 && numberOfFilms < 30){
+    console.log("Вы класический зритель!");
+} else if(numberOfFilms >= 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка!")
+};
+
 for(let i = 0; i < 2; i++){
     const filmsWatched = prompt("Один из последних просмотренных фильмов?");
     const gradeFilm = prompt("Оцените этот фильм?");
-    personalMovieDB.movie[filmsWatched] = gradeFilm;
+
+    if(filmsWatched != null && gradeFilm != null && filmsWatched != "" && gradeFilm != "" && filmsWatched.length < 50) {
+        personalMovieDB.movie[filmsWatched] = gradeFilm;
+        console.log("done!")
+    } else {
+        console.log("error!");
+        i--;
+    };
 };
 
 console.log(personalMovieDB);
